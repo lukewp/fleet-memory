f45008510bf365f010870da9ee6c871ba75afbf8
Luke <luke@hayloftadvisors.com>
2026-06-21 10:10:25 -0400

    Pass --dir /data/brain to dream for filesystem phases

--- systemd/gbrain-dream.sh
+++ systemd/gbrain-dream.sh
@@ -14,7 +14,7 @@ GBRAIN_USER=$(stat -c '%U' /data/brain)
 sudo -u "$GBRAIN_USER" bash -c '
   cd /data/gbrain
   set -a; source /data/fleet-memory/.env; set +a
-  ~/.bun/bin/bun run src/cli.ts dream --json
+  ~/.bun/bin/bun run src/cli.ts dream --json --dir /data/brain
 ' 2>&1 | logger -t "$LOG_TAG"
 DREAM_EXIT=$?
 
